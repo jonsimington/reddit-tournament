bda336c992c118836f055920f62f8972a261849b
Jon Simington <jon.simington@mst.edu>
2015-10-23 02:22:35 -0500

    Add profiles and allauth urls

--- tournament/tournament/urls.py
+++ tournament/tournament/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import logout_then_login
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('tournament.home.urls')),
+    url(r'^', include('tournament.profiles.urls')),
+
+    # Django AllAuth
+    url(r'^accounts/logout/$', logout_then_login),
+    url(r'^accounts/', include('allauth.urls')),
 ]
